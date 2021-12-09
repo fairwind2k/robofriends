2f69a52193b447854d204ca599d0374df5579de4
fairwind2k <fairwind@md2k.net>
2021-12-09 16:32:46 +0100

    Create cardList

--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Card = ({name, email, id}) => {
      return (
-        <div className='bg-light-blue dib br3 pa3 ma2 grow bw shadow-5'>
+        <div className='bg-light-blue tc dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
             <div>
-                <h2>{name}</h2>
+                <h4>{name}</h4>
                 <p>{email}</p>
             </div>
         </div>
